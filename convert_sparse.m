e4b5da8cedd609831558ddd0ea186e4b635c57cb
David <dgleich@stanford.edu>
2009-03-20 17:20:57 -0700

    Wrapped sparse_to_csr in a handy convert_sparse function for easily getting the necessary structure.:

---
+++ convert_sparse.m
@@ -0,0 +1,16 @@
+function As = convert_sparse(A)
+% CONVERT_SPARSE Convert a sparse matrix to the native gaimc representation
+%
+% As = convert_sparse(A) returns a struct with the three arrays defining
+% the compressed sparse row structure of A.
+%
+% Example:
+%   load('graphs/all_shortest_paths_example')
+%   As = convert_sparse(A)
+%
+% See also SPARSE_TO_CSR SPARSE
+
+[rp ci ai] = sparse_to_csr(A);
+As.rp = rp;
+As.ci = ci;
+As.ai = ai;
