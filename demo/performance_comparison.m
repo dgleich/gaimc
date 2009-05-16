ec40e369e67f6eb4889ab174143c9d3ed5829e6c
David Gleich <dgleich@stanford.edu>
2009-05-16 15:09:20 -0700

    Fixed some typos and removed a useless plot of performance as a function of graph time

--- demo/performance_comparison.m
+++ demo/performance_comparison.m
@@ -107,9 +107,11 @@ results(end).mat_std = mat_std;
 
 %%
 % Plot the data for connected components
-plot(szs, comp_results,'.-'); 
-legend('mex fast','mat fast','mex std','mat std','Location','Northwest');
-ylabel('time'); xlabel('graph size');
+% This plot isn't too meaningful, so I've omitted it.
+
+% plot(szs, comp_results,'.-'); 
+% legend('mex fast','mat fast','mex std','mat std','Location','Northwest');
+% ylabel('time'); xlabel('graph size');
 
 %% Dijkstra's algorithm
 % To evaluate the performance of Dijkstra's algorithm, we pick 
@@ -241,7 +243,7 @@ results(end).mat_std = mat_std;
 
 %% Summarize the results
 % We are going to summarize the results in a bar plot based on the
-% algorithm.  Each algorithm is a single barcopyfile(['..' filesep 'dfs.m'],'dfstest.m');
+% algorithm.  Each algorithm is a single bar
 graphs = {'all_shortest_paths_example', 'clr-24-1', 'cs-stanford', ...
     'minnesota','tapir'};, where the performance of the
 % mex code is 1.  
